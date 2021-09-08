3f0941a5cdb8d84a6742e4f8881343495429f615
Ginger Alford <ginger.alford@ruralsourcing.com>
2021-09-08 18:22:34 -0400

    OdeToFood Tutorial, section 2

---
+++ OdeToFood.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OdeToFood.Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                // /home/contact/1
+                //it will look for homeController, then a method named Contact, and you can include an id, but it's optional.
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //the default of every controller is Index, as specified above. Id is optional.
+            );
+        }
+    }
+}
---
+++ OdeToFood.Web/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using OdeToFood.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        IRestaurantData db;
+        //private field of type IRestaurantData. We had to add a reference here. We also had the add the "using" statement
+        //to the top of the file.
+
+        public HomeController()
+        {
+            db = new InMemoryRestaurantData();
+            //retrieving an object that implements the IRestaurantData interface.
+            //constructs a new instance of this datatype
+        }
+        public ActionResult Index()
+        {
+            //what does Index() return? an ActionResult
+            var model = db.GetAll();
+            //return "Hello World!";
+            //This Index() method returns what will appear on Index
+            return View(model);
+            //when you render a view from a HomeController, the framework will look in the Views/Home folder, 
+            //and for the Index.cshtml inside there(a RazorView)
+            //we are passing an IEnumerable of Restaurant to that Index.cshtml view
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
